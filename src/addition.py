02c15e2c0a606f1f19849ef1a1b9c91738c1d055
Sai marthala <saimarthala0799@gmail.com>
2024-07-16 22:01:39 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,11 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+# app.py
+# This is a test commit
+def sub(a, b):
+    return a - b
+
+def test_sub():
+    assert sub(1, 2) == 3
+    assert sub(1, -1) == 0
